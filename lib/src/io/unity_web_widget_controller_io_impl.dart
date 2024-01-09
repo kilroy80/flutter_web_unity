476477aab4a4511ae37fcc50b4dd2cd4d288600f
kilroy80 <kilroy80@gmail.com>
2024-01-09 14:08:11 +0900

    feat: upgrade code & change naming

--- lib/src/io/unity_web_widget_controller_io_impl.dart
+++ lib/src/io/unity_web_widget_controller_io_impl.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_web_unity/src/unity_web_widget_controller.dart';
 
-class HtmlWebWidgetControllerIoImpl extends UnityWebWidgetController {
+class UnityWebWidgetControllerIoImpl extends UnityWebWidgetController {
   @override
   Future<void> quit() async {
   }
