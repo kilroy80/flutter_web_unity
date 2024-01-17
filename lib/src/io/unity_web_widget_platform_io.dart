0734e486fdb5a952d20668839e4a41e6167fc500
kilroy80 <kilroy80@gmail.com>
2024-01-17 12:12:17 +0900

    feat: code update

--- lib/src/io/unity_web_widget_platform_io.dart
+++ lib/src/io/unity_web_widget_platform_io.dart
@@ -6,10 +6,16 @@ class UnityWebWidget extends StatefulWidget {
     super.key,
     required this.data,
     this.onWidgetCreate,
+    this.onUnityMessage,
+    this.onUnitySceneLoaded,
+    this.useCanvas = false,
   });
 
   final Map<String, dynamic> data;
   final Function(UnityWebWidgetController controller)? onWidgetCreate;
+  final Function(String message)? onUnityMessage;
+  final Function(String message)? onUnitySceneLoaded;
+  final bool? useCanvas;
 
   @override
   State<UnityWebWidget> createState() => _UnityWebWidgetState();
